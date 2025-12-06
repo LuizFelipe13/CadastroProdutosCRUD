a262dd2c6fed8820fe8fe3a66cb09bc1f7fca109
LuizFelipe13 <luiz_felipesbc@hotmail.com>
2025-12-06 20:24:16 -0300

    Criação de uma console simples que faz um CRUD em um banco "fake" na classe ProdutoRepositorio.cs (#1)

---
+++ CadastroProdutosCRUD/ProdutoRepositorio.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroProdutosCRUD
+{
+    public class ProdutoRepositorio
+    {
+        private List<Produto> produtos = new List<Produto>();
+        private int proximoId = 1;
+
+        public void Criar(Produto produto)
+        {
+            produto.Id = proximoId++;
+            produtos.Add(produto);
+        }
+
+        public List<Produto> Listar()
+        {
+                       return produtos;
+        }
+
+        public Produto BuscarPorId(int id)
+        {
+            return produtos.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool Editar (int id, string novoNome, decimal novoPreco)
+        {
+            var produto = BuscarPorId(id);
+            if (produto == null) return false;
+
+            produto.Nome = novoNome;
+            produto.Preco = novoPreco;
+
+            return true;
+
+        }
+
+        public bool Deletar(int id)
+        {
+            var produto = BuscarPorId(id);
+            return produtos.Remove(produto);
+        }
+    }
+}
--- CadastroProdutosCRUD/Program.cs
+++ CadastroProdutosCRUD/Program.cs
@@ -1,2 +1,105 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CadastroProdutosCRUD;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ProdutoRepositorio repo = new ProdutoRepositorio();
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("=== SISTEMA DE CADASTRO DE PRODUTOS ===");
+            Console.WriteLine("1 - Criar produto");
+            Console.WriteLine("2 - Listar produtos");
+            Console.WriteLine("3 - Editar produto");
+            Console.WriteLine("4 - Deletar produto");
+            Console.WriteLine("0 - Sair");
+            Console.Write("Escolha uma opção: ");
+
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    CriarProduto(repo);
+                    break;
+                case "2":
+                    ListarProdutos(repo);
+                    break;
+                case "3":
+                    EditarProduto(repo);
+                    break;
+                case "4":
+                    DeletarProduto(repo);
+                    break;
+                case "0":
+                    return;
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+
+            Console.WriteLine("\nPressione ENTER para continuar...");
+            Console.ReadLine();
+        }
+    }
+
+    static void CriarProduto(ProdutoRepositorio repo)
+    {
+        Console.Write("Nome do produto: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Preço: ");
+        decimal preco = decimal.Parse(Console.ReadLine());
+
+        repo.Criar(new Produto { Nome = nome, Preco = preco });
+
+        Console.WriteLine("Produto criado com sucesso!");
+    }
+
+    static void ListarProdutos(ProdutoRepositorio repo)
+    {
+        var produtos = repo.Listar();
+
+        if (produtos.Count == 0)
+        {
+            Console.WriteLine("Nenhum produto cadastrado.");
+            return;
+        }
+
+        foreach (var p in produtos)
+        {
+            Console.WriteLine($"ID: {p.Id} | Nome: {p.Nome} | Preço: R$ {p.Preco}");
+        }
+    }
+
+    static void EditarProduto(ProdutoRepositorio repo)
+    {
+        Console.Write("Informe o ID do produto a editar: ");
+        int id = int.Parse(Console.ReadLine());
+
+        Console.Write("Novo nome: ");
+        string novoNome = Console.ReadLine();
+
+        Console.Write("Novo preço: ");
+        decimal novoPreco = decimal.Parse(Console.ReadLine());
+
+        if (repo.Editar(id, novoNome, novoPreco))
+            Console.WriteLine("Produto editado com sucesso!");
+        else
+            Console.WriteLine("Produto não encontrado.");
+    }
+
+    static void DeletarProduto(ProdutoRepositorio repo)
+    {
+        Console.Write("ID do produto para deletar: ");
+        int id = int.Parse(Console.ReadLine());
+
+        if (repo.Deletar(id))
+            Console.WriteLine("Produto deletado com sucesso!");
+        else
+            Console.WriteLine("Produto não encontrado.");
+    }
+}
